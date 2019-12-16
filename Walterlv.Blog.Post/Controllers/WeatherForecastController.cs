f2aa448294a6171aed2022d98a73f08390ace4bd
walterlv <xblero@gmail.com>
2019-12-16 22:59:20 +0800

    Extract the model project.

--- Walterlv.Blog.Post/Controllers/WeatherForecastController.cs
+++ Walterlv.Blog.Post/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Walterlv.Blog.Data;
 
 namespace Walterlv.Blog.Controllers
 {
