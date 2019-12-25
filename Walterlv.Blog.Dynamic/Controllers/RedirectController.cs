e0411912e322d774bc01d9fe41a6f7cacd6123f3
walterlv <xblero@gmail.com>
2019-12-25 21:59:17 +0800

    import blog.

---
+++ Walterlv.Blog.Dynamic/Controllers/RedirectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Walterlv.BlogBanners.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class RedirectController : ControllerBase
+    {
+        [HttpGet, Route("csdn/column.png")]
+        public IActionResult UrlMove()
+        {
+            return Redirect("https://www.zhipin.com/job_detail/26bf2e69fc65103d1HN539S_FFQ~.html");
+        }
+
+        [HttpGet, Route("blog/header.png")]
+        public IActionResult BulletinUrlMove()
+        {
+            return Redirect("https://blog.walterlv.com");
+        }
+
+        [HttpGet, Route("blog/footer.png")]
+        public IActionResult BannerUrlMove()
+        {
+            return Redirect("https://www.zhipin.com/job_detail/26bf2e69fc65103d1HN539S_FFQ~.html");
+        }
+    }
+}
