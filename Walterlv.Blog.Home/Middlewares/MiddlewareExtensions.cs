ff4761fa5fdf72964ac417ff0d050903f87ce11a
walterlv <xblero@gmail.com>
2019-12-28 20:22:16 +0800

    Add auto remove html middleware

---
+++ Walterlv.Blog.Home/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace Walterlv.Blog.Middlewares
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExternalHttpsRedirection(this IApplicationBuilder app) => app.Use(async (context, next) =>
+        {
+            if (!context.Request.IsHttps
+                && "localhost" != context.Request.Host.Host
+                && "127.0.0.1" != context.Request.Host.Host
+                && "[::1]" != context.Request.Host.Host)
+            {
+                // HTTP 重定向为 HTTPS
+                var url = "https://" + context.Request.Host + context.Request.PathBase + context.Request.Path;
+                context.Response.Redirect(url);
+                context.Response.StatusCode = 301;
+                return;
+            }
+            await next().ConfigureAwait(false);
+        });
+
+        public static IApplicationBuilder UseAutoRemoveHtmlExtension(this IApplicationBuilder app) => app.Use(async (context, next) =>
+        {
+            if (context.Request.Path.HasValue
+                && context.Request.Path.Value.EndsWith(".html", StringComparison.OrdinalIgnoreCase))
+            {
+                // 去掉 .html 后缀
+                var url = "https://" + context.Request.Host + context.Request.PathBase + context.Request.Path;
+                context.Response.Redirect(url);
+                context.Response.StatusCode = 301;
+                return;
+            }
+            await next().ConfigureAwait(false);
+        });
+    }
+}
